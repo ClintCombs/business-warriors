d347dd43e83f8ea2932fe27e22006b3f62a59328
Clint Combs <clint@ccombs.net>
2015-02-23 23:03:21 -0500

    #1: Updated domain and serialization.

--- src/main/scala/buswarriors/JsonRendering.scala
+++ src/main/scala/buswarriors/JsonRendering.scala
@@ -12,12 +12,33 @@ trait JsonRendering extends Json4sJacksonSupport {
   import FieldSerializer._
 
   val productInventorySerializer =
-    FieldSerializer[ProductInventory](renameTo("quantity", "qty"))
+    FieldSerializer[ProductInventory](renameTo("quantity", "qty"), renameFrom("qty", "quantity"))
   val transactionSerializer =
-    FieldSerializer[Transaction](renameTo("quantity", "qty"))
+    FieldSerializer[Transaction](renameTo("quantity", "qty"), renameFrom("qty", "quantity"))
+  class PriceSerializer extends CustomSerializer[Price](format => (
+      { case JDecimal(d) => new Price(d) }, { case price: Price => JDecimal(price.p) }
+    ))
+  class QuantitySerializer extends CustomSerializer[Quantity](format => (
+      { case JInt(i) => new Quantity(i.toInt) }, { case quantity: Quantity => JInt(quantity.q) }
+    ))
+  class ProductInventorySerializer extends CustomSerializer[ProductInventory](format => (
+      {
+        case JObject(
+            JField("sku", JString(s)) :: JField("name", JString(n)) ::
+              JField("price", JDecimal(p)) :: JField("qty", JInt(q)) :: Nil) =>
+          ProductInventory(Product(s, n, p), q) },
+      {
+        case pi: ProductInventory =>
+          JObject(
+            JField("sku", JString(pi.product.sku)) :: JField("name", JString(pi.product.name)) ::
+              JField("price", JDecimal(pi.product.price.p)) :: JField("qty", JInt(pi.quantity.q)) :: Nil)
+      }
+    ))
 
   implicit override def json4sJacksonFormats: Formats =
-    DefaultFormats + productInventorySerializer + transactionSerializer
+    DefaultFormats +
+      productInventorySerializer + transactionSerializer +
+      new PriceSerializer() + new QuantitySerializer() + new ProductInventorySerializer()
 
   def renderRoot: String = {
     pretty("system" -> "business-warriors")
--- src/main/scala/buswarriors/package.scala
+++ src/main/scala/buswarriors/package.scala
@@ -1,6 +1,30 @@
 
 package object buswarriors {
-  case class Product(sku: String, name: String, price: BigDecimal)
-  case class ProductInventory(product: Product, quantity: Int)
-  case class Transaction(product: Product, quantity: Int, total: BigDecimal)
+
+  type SKU = String
+
+  type TransactionId = String
+
+  implicit class Quantity(val q: Int) { require(q >= 0, s"${q} is less than zero") }
+
+  implicit def quantity(q: BigInt) = Quantity(q.toInt)
+
+  implicit class Price(val p: BigDecimal) { require(p >= 0.0, s"${p} is less than zero") }
+
+  implicit def price(d: Double) = Price(BigDecimal(d))
+
+  case class Product(sku: SKU, name: String, price: Price) {
+    require(sku != null, "sku is null")
+    require(name != null, "name is null")
+  }
+
+  case class ProductInventory(product: Product, quantity: Quantity)
+
+  case class LineItem(sku: SKU, quantity: Quantity, name: String, price: Price, subtotal: Price)
+
+  case class Transaction(transId: TransactionId, total: Price, lineItems: Seq[LineItem]) {
+    require(
+      total.p == lineItems.map(_.price.p).sum,
+      s"transaction total, ${total}, not equal to line item total, ${lineItems.map(_.price.p).sum}")
+  }
 }
